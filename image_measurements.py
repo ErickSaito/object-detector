1c8649140c76dc68f0b296060db00e46ef4262b1
Erick Saito <saitoerick@gmail.com>
2022-11-10 23:01:52 -0300

    change tranformation to gray image

--- image_measurements.py
+++ image_measurements.py
@@ -8,19 +8,21 @@ class ImageMeasurements():
 
   def color_tranformation(self):
     image = self._manager.get_image()
-    black_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    self._manager.save_image("color_tranform", black_image)
+    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    ret, thresh = cv.threshold(gray_image, 200, 255, 0)
+    self._manager.save_image("color_tranform", thresh)
   
   def border_detection(self):
     image = self._manager.get_image()
     gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    image_border = cv.Laplacian(gray_image, cv.CV_64F)
+    ret, thresh = cv.threshold(gray_image, 200, 255, 0)
+    image_border = cv.Laplacian(thresh, cv.CV_64F)
     image_border[image_border == 0] = 255
     self._manager.save_image('border', image_border)
   
   def properties(self):
     image = self._manager.get_image()
     gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    ret,  thresh = cv.threshold(gray_image, 127, 255, 0)
+    ret,  thresh = cv.threshold(gray_image, 200, 255, 0)
     contours, hierarchy = cv.findContours(thresh, 1, 2)
     print(len(contours))
