4b097ececb288b14638093b7528147417a41bbd0
Erick Saito <saitoerick@gmail.com>
2022-11-11 00:27:52 -0300

    add area and perimeter of contours

--- image_measurements.py
+++ image_measurements.py
@@ -25,4 +25,18 @@ class ImageMeasurements():
     gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     ret,  thresh = cv.threshold(gray_image, 200, 255, 0)
     contours, hierarchy = cv.findContours(thresh, 1, 2)
-    print(len(contours))
+
+    print(f"número de regiões: {str(len(contours) - 1)}")
+    for i, c in enumerate(contours):
+      if (i < len(contours) - 1):
+        M = cv.moments(c)
+        
+        area = cv.contourArea(c)
+        perimeter = cv.arcLength(c, True)
+
+        print(f"região {str(i)}: área: {int(area)} perímetro: {round(perimeter, 6)}")
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])  
+        cv.putText(thresh, str(i), (cX, cY), cv.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+    
+    self._manager.save_image('properties', thresh)
